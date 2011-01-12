b385918847df94bdbb7a92f29c6fd759538d9645
Mark Swayne <toad@Toad-iMac.local>
2011-01-11 16:09:40 -0800

    Got things mostly working.  Started adding primary objects.

---
+++ Point.pm
@@ -0,0 +1,37 @@
+package Point;
+
+use Moose;
+use Carp qw( croak );
+
+use overload '""', \&as_string;
+
+has [qw(
+    x
+    y
+)] => (
+    is  => 'ro',
+    isa => 'Int',
+    required => 1,
+);
+
+
+sub distance {
+    my $class = shift if @_ == 3;
+    my $source = shift;
+    my $dest   = shift;
+
+    croak "Both source and destination must be Point objects"
+        unless $source->isa('Point') and $dest->isa('Point');
+
+    my $dist = sqrt( ($source->x - $dest->x)**2 + ($source->y - $dest->y)**2 );
+
+    return $dist;
+}
+
+sub as_string {
+    my $self = shift;
+
+    return sprintf "(%.3d,%.3d)", $self->x, $self->y;
+}
+
+1;
---
+++ Resource.pm
@@ -0,0 +1,113 @@
+package Resource;
+
+use Moose;
+
+use Carp qw( croak );
+
+use overload '""', \&as_string;
+
+has type => (
+    is     => 'ro',
+    isa    => 'Str',
+    required => 1,
+);
+
+has stored => (
+    is     => 'ro',
+    isa    => 'Int',
+    required => 1,
+);
+
+has per_hour => (
+    is     => 'ro',
+    isa    => 'Int',
+    required => 1,
+);
+
+has capacity => (
+    is     => 'ro',
+    isa    => 'Int',
+    predicate => 'has_capacity',
+);
+
+
+
+sub percent_capacity {
+    my $self = shift;
+
+    my $type = $self->type;
+
+    croak "resource $type must have capacity to calculate percent_capacity()"
+        unless $self->has_capacity;
+
+    my $s = $self->stored;
+    my $h = $self->per_hour;
+    my $c = $self->capacity;
+
+    my $p = int( 100 * $s / $c );
+
+    return $p;
+}
+
+sub full {
+    my $self = shift;
+
+    my $type = $self->type;
+
+    croak "resource $type must have capacity to be full"
+        unless $self->has_capacity;
+
+    my $s = $self->stored;
+    my $c = $self->capacity;
+
+    return $s == $c;
+}
+
+sub full_in {
+    my $self = shift;
+
+    my $type = $self->type;
+
+    croak "resource $type  must have capacity to calculate full_in()"
+        unless $self->has_capacity;
+
+    my $s = $self->stored;
+    my $h = $self->per_hour;
+    my $c = $self->capacity;
+
+    return if $h < 0;
+
+    my $left = $c - $s;
+    
+    my $time_left = $left / $h; # hours
+
+    return $time_left / 3600; # seconds
+}
+
+sub empty_in {
+    my $self = shift;
+
+    my $type = $self->type;
+
+    my $s = $self->stored;
+    my $h = $self->per_hour;
+
+    return if $h < 0;
+
+    my $time_left = $s / $h; # hours
+
+    return $time_left / 3600; # seconds
+}
+
+sub as_string {
+    my $self = shift;
+
+
+    return sprintf "%s: %0d / %0d at %0d per hour", $self->type, $self->stored, $self->capacity, $self->per_hour
+        if $self->has_capacity;
+
+    return sprintf "%s: %0d at %0d per hour", $self->type, $self->stored, $self->per_hour
+}
+
+
+1;
